c2d411fbef369005141b2fec6599a86906e329a6
datta <karanamdatta.98@gmail.com>
2022-06-15 17:24:41 +0530

    Chrome Browser Project
    Signed-off-by: datta <karanamdatta.98@gmail.com>

---
+++ browser.java
@@ -0,0 +1,14 @@
+
+public class browser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty(
+	            "webdriver.chrome.driver",
+	            "C:\\Users\\ADMIN\\Documents\\chromedriver.exe");
+	        // Instantiate a ChromeDriver class.
+	        WebDriver driver = new ChromeDriver();
+        
+	}
+
+}
